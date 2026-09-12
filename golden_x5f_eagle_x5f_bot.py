cf46cbee12d91b4717ab2dcc8b85515d1f478960
khaled-earn <khaledalaanzy51@gmail.com>
2026-09-12 22:52:58 +0300

    Update golden_x5f_eagle_x5f_bot.py

--- golden_x5f_eagle_x5f_bot.py
+++ golden_x5f_eagle_x5f_bot.py
@@ -1,4 +1,21 @@
+from flask import Flask
+from threading import Thread
 import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 import asyncio
 import requests
 from telegram import Update
